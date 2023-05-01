ea1edb16c5e86622d6b98194630536883115b367
shareefshareef <132113151+shareefshareef@users.noreply.github.com>
2023-05-01 10:30:59 +0530

    fifth commit

--- dom.js
+++ dom.js
@@ -23,6 +23,7 @@ for(let i = 0;i<items.length;i++){
     items[i].style.backgroundColor = "yellow";
 }
 */
+/*
 let clasedit = document.getElementsByClassName("list-group");
 clasedit.textContent="user unknown";
 
@@ -35,5 +36,25 @@ for(let i=0;i<li.length;i++){
 for(let i=0;i<li.length;i++){
     li[i].style.backgroundColor="red";
 };
+*/
+let changeitem = document.querySelector(".list-group-item:nth-child(2)")
+changeitem.style.backgroundColor="green"
+
+let changeitem2 = document.querySelector(".list-group-item:nth-child(3)")
+changeitem2.style.display = "none"
+
+
+let make_font_color_green = document.querySelectorAll("li")
+
+make_font_color_green[1].style.color = "green"
+
+
+let odd = document.querySelectorAll("li:nth-child(odd)");
+
+
+for(let i = 0;i<odd.length;i++){
+odd[i].style.backgroundColor="green"
+}
+//second element is green because i changed the element to be green and the third element has no value because it set the value to be invinsible
 
 
